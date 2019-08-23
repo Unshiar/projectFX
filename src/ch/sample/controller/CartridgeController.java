f74b188d0def43f0ea1f63da6b3862b2a3f9dacf
Unshiar <sergosr.ryab@yandex.ru>
2019-08-23 15:46:36 +0300

    Basic logic and view for Cartridge model.

--- src/ch/sample/controller/CartridgeController.java
+++ src/ch/sample/controller/CartridgeController.java
@@ -1,10 +1,48 @@
 package ch.sample.controller;
 
 import ch.sample.model.Cartridge;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
 
 public class CartridgeController {
 
     private boolean resultPresents = false;
     private Cartridge cartridge;
 
+    @FXML
+    private TextField textField_CurrentNumber;
+
+    @FXML
+    private TextField textField_NewNumber;
+
+    @FXML
+    private TextField textField_CurrentModel;
+
+    @FXML
+    private TextField textField_NewModel;
+
+    public CartridgeController() {
+        this.cartridge = new Cartridge();
+    }
+
+    public CartridgeController(Cartridge cartridge) {
+        this.cartridge = cartridge;
+    }
+
+    public Cartridge getCartridge() {
+        return cartridge;
+    }
+
+    public void setCartridge(Cartridge cartridge) {
+        this.cartridge = cartridge;
+    }
+
+    public boolean isResultPresents() {
+        return resultPresents;
+    }
+
+    @FXML
+    private void initialize() {
+
+    }
 }
