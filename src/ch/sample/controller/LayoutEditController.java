fb62106385486055b05b31127fd9e82015ba61f8
Ryabinchuk Sergey <sergosr.ryab@yandex.ru>
2019-08-25 14:38:48 +0300

    Handle Add, Edit, Remove, GetAll for Cartridge

--- src/ch/sample/controller/LayoutEditController.java
+++ src/ch/sample/controller/LayoutEditController.java
@@ -1,7 +1,9 @@
 package ch.sample.controller;
 
 import ch.sample.Main;
+import ch.sample.model.Cartridge;
 import ch.sample.model.UserModel;
+import ch.sample.services.CartridgeService;
 import ch.sample.services.UserService;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
@@ -15,6 +17,8 @@ import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
 import javafx.stage.WindowEvent;
+
+import java.io.IOException;
 
 public class LayoutEditController {
 
@@ -32,6 +36,12 @@ public class LayoutEditController {
     private Button buttonEditUser;//кнопка изменить сотрудника
 
     @FXML
+    private TableView<UserModel> tableViewUsers;//таблица с пользователями
+
+    @FXML
+    private TableColumn<UserModel, String> tableColumnUserName;//колонка таблицы - имя пользователя
+
+    @FXML
     private Button buttonAddCartridge;//кнопка добавить картридж
 
     @FXML
@@ -39,6 +49,12 @@ public class LayoutEditController {
 
     @FXML
     private Button buttonEditCartridge;//кнопка изменить картридж
+
+    @FXML
+    TableView<Cartridge> tableViewCartridges;//таблица с картриджами
+
+    @FXML
+    TableColumn<Cartridge, String> columnCartridgeNumber;//колонка таблицы - номер картриджа
 
     @FXML
     private Button buttonAddDefect;//кнопка добавить неисправность
@@ -49,13 +65,8 @@ public class LayoutEditController {
     @FXML
     private Button buttonEditDefect;//кнопка изменить неисправность
 
-    @FXML
-    private TableView<UserModel> tableViewUsers;//таблица с пользователями
-
-    @FXML
-    private TableColumn<UserModel, String> tableColumnUserName;//колонка имя пользователя
-
-    private ObservableList<UserModel> usersList = FXCollections.observableArrayList();
+    private ObservableList<UserModel> usersList = FXCollections.observableArrayList();//список пользователей
+    private ObservableList<Cartridge> cartridgesList = FXCollections.observableArrayList();//список картриджей
 
     public LayoutEditController() {
 
@@ -63,8 +74,14 @@ public class LayoutEditController {
 
     @FXML
     private void initialize() {
+        //Сопоставляем колонке "Имя пользователя", свойство "userName" из модели UserModel
         tableColumnUserName.setCellValueFactory(new PropertyValueFactory<>("userName"));
+        //заполняем таблицу с Сотрудниками(пользователями)
         tableViewUsers.setItems(usersList);
+        //Сопоставляем колонке "Номер картриджа", свойство "number" из модели Cartridge
+        columnCartridgeNumber.setCellValueFactory(new PropertyValueFactory<>("number"));
+        //заполняем таблицу с Картриджами
+        tableViewCartridges.setItems(cartridgesList);
 
         //ToolTips for buttons
         Tooltip tooltipAdd = new Tooltip("Добавить");
@@ -87,7 +104,7 @@ public class LayoutEditController {
 
     //добавляем нового пользователя в базу и список отображения
     @FXML
-    public void handleAddUser() throws Exception {
+    public void handleAddUser() throws IOException {
         Stage stage = new Stage();
         stage.setTitle("Добавление нового пользователя");
         stage.initModality(Modality.APPLICATION_MODAL);//модальное окно
@@ -158,5 +175,96 @@ public class LayoutEditController {
         UserService userService = new UserService();
         usersList = FXCollections.observableArrayList(userService.findAllUsers());
         tableViewUsers.setItems(usersList);
+    }
+
+    //добавляем новый картридж в базу данных и список отображения
+    @FXML
+    public void handleAddCartridge() {
+        Stage stage = new Stage();
+        stage.setTitle("Добавление нового картриджа");
+        stage.initModality(Modality.APPLICATION_MODAL);//модальное окно
+
+        FXMLLoader loader = new FXMLLoader();
+        CartridgeController cartridgeController = new CartridgeController();
+        loader.setController(cartridgeController);
+        String viewLocation = "view/CartridgeView.fxml";
+        loader.setLocation(Main.class.getResource(viewLocation));
+        Parent parent;
+        try {
+            parent = loader.load();
+        } catch (Exception ex) {
+            System.err.println("Can't load " + viewLocation);
+            ex.printStackTrace();
+            return;
+        }
+        stage.setScene(new Scene(parent));
+        stage.setResizable(false);
+
+        stage.setOnHidden(new EventHandler<WindowEvent>() {
+            @Override
+            public void handle(WindowEvent event) {
+                if(cartridgeController.isResultPresents()) {
+                    CartridgeService cartridgeService = new CartridgeService();
+                    cartridgeService.saveCartridge(cartridgeController.getCartridge());
+                    cartridgesList.add(cartridgeController.getCartridge());
+                }
+            }
+        });
+
+        stage.showAndWait();
+    }
+
+    //Удаляем картридж из базы данных и обновляем в таблице UI
+    @FXML
+    public void handleRemoveCartridge() {
+        CartridgeService cartridgeService = new CartridgeService();
+        cartridgeService.deleteCartridge(tableViewCartridges.getSelectionModel().getSelectedItem());
+        tableViewCartridges.getItems().remove(tableViewCartridges.getSelectionModel().getSelectedItem());
+    }
+
+    //Редактируем выбраный картридж
+    @FXML
+    public void handleEditCartridge() {
+        Stage stage = new Stage();
+        stage.setTitle("Редактирование картриджа");
+        stage.initModality(Modality.APPLICATION_MODAL);//модальное окно
+
+        FXMLLoader loader = new FXMLLoader();
+        Cartridge cartridge = tableViewCartridges.getSelectionModel().getSelectedItem();
+        CartridgeController cartridgeController = new CartridgeController(cartridge);
+        loader.setController(cartridgeController);
+        String viewLocation = "view/CartridgeView.fxml";
+        loader.setLocation(Main.class.getResource(viewLocation));
+        Parent parent;
+        try {
+            parent = loader.load();
+        } catch (Exception ex) {
+            System.err.println("Can't load " + viewLocation);
+            ex.printStackTrace();
+            return;
+        }
+        stage.setScene(new Scene(parent));
+        stage.setResizable(false);
+
+        stage.setOnHidden(new EventHandler<WindowEvent>() {
+            @Override
+            public void handle(WindowEvent event) {
+                if(cartridgeController.isResultPresents()) {
+                    tableViewCartridges.refresh();
+                    CartridgeService cartridgeService = new CartridgeService();
+                    cartridgeService.updateCartridge(cartridgeController.getCartridge());
+                }
+            }
+        });
+
+        stage.showAndWait();
+    }
+
+    //получаем все картриджи из базы данных и отображаем в таблице UI
+    @FXML
+    public void handleGetAllCartridges() {
+        CartridgeService cartridgeService = new CartridgeService();
+        cartridgesList = FXCollections.observableArrayList(cartridgeService.findAllCartridges());
+        tableViewCartridges.setItems(cartridgesList);
     }
 }
