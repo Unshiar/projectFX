82769a8f169c0f9988389ed0c85ad7e689eb258c
Unshiar <sergosr.ryab@yandex.ru>
2019-08-26 17:00:14 +0300

    Controller for Defect model

--- src/ch/sample/controller/DefectController.java
+++ src/ch/sample/controller/DefectController.java
@@ -1,9 +1,64 @@
 package ch.sample.controller;
 
 import ch.sample.model.Defect;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
 
 public class DefectController {
 
     private boolean resultPresents = false;
     private Defect defect;
+
+    @FXML
+    TextField textField_CurrentName;
+
+    @FXML
+    TextField textField_NewName;
+
+    @FXML
+    Button buttonSave;
+
+    @FXML
+    Button buttonCancel;
+
+    public DefectController() {
+        this.defect = new Defect();
+    }
+
+    public DefectController(Defect defect) {
+        this.defect = defect;
+    }
+
+    public Defect getDefect() {
+        return defect;
+    }
+
+    public void setDefect(Defect defect) {
+        this.defect = defect;
+    }
+
+    public boolean isResultPresents() {
+        return resultPresents;
+    }
+
+    @FXML
+    private void initialize() {
+        textField_CurrentName.setText(defect.getName());
+    }
+
+    @FXML
+    private void handleSave() {
+        defect.setName(textField_NewName.getText());
+        resultPresents = true;
+        Stage stage = (Stage)buttonSave.getScene().getWindow();
+        stage.close();
+    }
+
+    @FXML
+    private void handleCancel() {
+        Stage stage = (Stage)buttonCancel.getScene().getWindow();
+        stage.close();
+    }
 }
