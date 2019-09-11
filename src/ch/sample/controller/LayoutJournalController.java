6806fc150908b79584b3240aec1e22e0011be0f4
Unshiar <sergosr.ryab@yandex.ru>
2019-09-11 16:08:44 +0300

    Fill table in window "Отчет" By pressed button "Обновить данные" get data from database and fill tableView

--- src/ch/sample/controller/LayoutJournalController.java
+++ src/ch/sample/controller/LayoutJournalController.java
@@ -3,24 +3,76 @@ package ch.sample.controller;
 import ch.sample.Main;
 import ch.sample.model.Report;
 import ch.sample.services.ReportService;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
 import javafx.stage.WindowEvent;
 
+import java.time.LocalDate;
+
 public class LayoutJournalController {
+
+    private ObservableList<Report> reportsList = FXCollections.observableArrayList();//список записей в журнале
+                                                                                    // учета расхода картриджей
+
+    @FXML
+    private TableView<Report> tableReports;//Таблица - журнал учета расхода картриджей
+
+    @FXML
+    private TableColumn<Report, String> columnNum;//колонка - "Номер"
+
+    @FXML
+    private TableColumn<Report, LocalDate> columnDate;//колонка - "Дата"
+
+    @FXML
+    private TableColumn<Report, String> columnUserName;//колонка - "ФИО"
+
+    @FXML
+    private TableColumn<Report, String> columnCartridgeOut;//колонка "Извлеч."
+
+    @FXML
+    private TableColumn<Report, String> columnCartridgeIn;//колонка "Установл."
+
+    @FXML
+    private TableColumn<Report, String> columnPrinterNum;//колонка "инв.№"
+
+    @FXML
+    private TableColumn<Report, String> columnDefectName;//колонка "Причина"
+
     @FXML
     private void initialize() {
-
+        //Заполняем таблицу учета расхода картриджейф
+        tableReports.setItems(reportsList);
+        //Сопоставим колонке - "Номер", свойсто "id" из модели Report
+        columnNum.setCellValueFactory(new PropertyValueFactory<>("id"));
+        //Сопоставим колонке - "Дата", свойсто "date" из модели Report
+        columnDate.setCellValueFactory(new PropertyValueFactory<>("date"));
+        //Сопоставим колонке - "ФИО", свойсто "userName" из модели Report
+        columnUserName.setCellValueFactory(new PropertyValueFactory<>("userName"));
+        //Сопоставим колонке - "Извлеч.", свойсто "cartridgeOut" из модели Report
+        columnCartridgeOut.setCellValueFactory(new PropertyValueFactory<>("cartridgeOut"));
+        //Сопоставим колонке - "Установл.", свойсто "cartridgeIn" из модели Report
+        columnCartridgeIn.setCellValueFactory(new PropertyValueFactory<>("cartridgeIn"));
+        //Сопоставим колонке - "инв.№", свойсто "printerNum" из модели Report
+        columnPrinterNum.setCellValueFactory(new PropertyValueFactory<>("printerNum"));
+        //Сопоставим колонке - "Причина", свойсто "defectName" из модели Report
+        columnDefectName.setCellValueFactory(new PropertyValueFactory<>("defectName"));
     }
 
     public LayoutJournalController() {
+
     }
 
+    @FXML
     public void handleCreateNote() {
         Stage stage = new Stage();
         stage.setTitle("Добавление новой записи");
@@ -54,5 +106,13 @@ public class LayoutJournalController {
         });
 
         stage.showAndWait();
+    }
+
+    //жмем кнопку "Обновить данные"
+    @FXML
+    public void handleRefreshData() {
+        ReportService reportService = new ReportService();
+        reportsList = FXCollections.observableArrayList(reportService.findAllReports());
+        tableReports.setItems(reportsList);
     }
 }
