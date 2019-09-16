1a09e89afe3bd52afc2f5ce99c5f6163d6f3ae12
Unshiar <sergosr.ryab@yandex.ru>
2019-09-16 11:07:35 +0300

    Added FXML elements Added FXML elements from LayoutJournal.fxml. Default values to datePickerStartDate and datePickerEndDate

--- src/ch/sample/controller/LayoutJournalController.java
+++ src/ch/sample/controller/LayoutJournalController.java
@@ -10,19 +10,45 @@ import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.TableColumn;
-import javafx.scene.control.TableView;
+import javafx.scene.control.*;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
 import javafx.stage.WindowEvent;
+import javafx.util.StringConverter;
 
 import java.time.LocalDate;
 
 public class LayoutJournalController {
 
     private ObservableList<Report> reportsList = FXCollections.observableArrayList();//список записей в журнале
-                                                                                    // учета расхода картриджей
+                                                                                  // учета расхода картриджей
+    @FXML
+    private RadioButton radioButtonAll;//кнопка - весь журнал
+
+    @FXML
+    private RadioButton radioButtonLast10;//кнопка - последние 10 записей
+
+    @FXML
+    private RadioButton radioButtonLastN;//кнопка - последние N записей
+
+    @FXML
+    private TextField textFieldLastN;//поле - количество последних записей
+
+    @FXML
+    private RadioButton radioButtonWeek;//кнопка - за неделю
+
+    @FXML
+    private RadioButton radioButtonMonth;//кнопка - за месяц
+
+    @FXML
+    private RadioButton radioButtonPeriod;//кнопка - за период
+
+    @FXML
+    private DatePicker datePickerStartDate;//поле - начальная дата
+
+    @FXML
+    private DatePicker datePickerEndDate;//поле - конечная дата
 
     @FXML
     private TableView<Report> tableReports;//Таблица - журнал учета расхода картриджей
@@ -50,6 +76,9 @@ public class LayoutJournalController {
 
     @FXML
     private void initialize() {
+        //начальные значения "За период" (по умолчанию текущий день)
+        datePickerStartDate.setValue(LocalDate.now());
+        datePickerEndDate.setValue(LocalDate.now());
         //Заполняем таблицу учета расхода картриджейф
         tableReports.setItems(reportsList);
         //Сопоставим колонке - "Номер", свойсто "id" из модели Report
